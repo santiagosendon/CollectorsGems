5ad3849f7fb4ce52fedfb905b6c9e7d6451fceec
github username <github email address>
2019-05-09 10:09:54 -0400

    adding log in

--- collectorsgems/src/index.js
+++ collectorsgems/src/index.js
@@ -10,3 +10,47 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+const baseUrl = 'http://localhost:3000'
+const loginForm = document.querySelector("#login-form")
+const usernameInput = document.querySelector("#username-input")
+const imageInput = document.querySelector("#image-input")
+const loginContainer = document.querySelector("#login-container")
+const mainContainer = document.querySelector(".main_container")
+const createImageButton = document.querySelector("#create-image-button")
+const userChatList = document.querySelector("#user-chat-list")
+const messageList = document.querySelector(".messages")
+const submitMessageButton = document.querySelector("#message-submit-button")
+const messageInput = document.querySelector("#create_message")
+let userImage = document.querySelector(".user_image")
+let userDisplayName = document.querySelector(".user_display_name")
+let chatSearchInput = document.querySelector("#chat_search_input")
+
+
+
+
+const state = {
+  users: null,
+  current_user: null,
+  current_chat: null,
+  other_chat_user: null,
+}
+
+// Login Page Setup Functions //
+//-------------------------//
+
+toggleImageInput = (event) => {
+      event.preventDefault()
+      imageInput.style.display == 'block' ? imageInput.style.display = 'none' : imageInput.style.display = 'block'
+    }
+
+createImageButton.addEventListener("click", toggleImageInput)
+
+loginSetup = () => {
+  fetch(baseUrl + "/users")
+    .then(response => response.json())
+    .then(data => {
+      state.users = data
+      loginForm.addEventListener("submit", logUserIn)
+  })
+}
